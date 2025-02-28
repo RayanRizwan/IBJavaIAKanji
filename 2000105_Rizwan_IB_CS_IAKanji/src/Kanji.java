a5c707152b439086d55811276c585a31aa6b302a
RayanRizwan <63744820+RayanRizwan@users.noreply.github.com>
2025-02-28 08:54:40 -0800

    Kanji
    nhuh

---
+++ 2000105_Rizwan_IB_CS_IAKanji/src/Kanji.java
@@ -0,0 +1,72 @@
+
+import java.util.ArrayList;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author 2000105
+ */
+public class Kanji {
+    int index;
+    String meaning;
+    ArrayList<String> Kunyomi;
+    ArrayList<String> Onyomi;
+    String level;
+    
+    public Kanji(int index, String meaning, ArrayList<String> Kunyomi, 
+            ArrayList<String> Onyomi, String Level){
+        index = this.index;
+        meaning = this.meaning;
+        Kunyomi = this.Kunyomi;
+        Onyomi = this.Onyomi;
+        Level = this.level;
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+
+    public ArrayList<String> getKunyomi() {
+        return Kunyomi;
+    }
+
+    public void setKunyomi(ArrayList<String> Kunyomi) {
+        this.Kunyomi = Kunyomi;
+    }
+
+    public ArrayList<String> getOnyomi() {
+        return Onyomi;
+    }
+
+    public void setOnyomi(ArrayList<String> Onyomi) {
+        this.Onyomi = Onyomi;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    
+    
+    
+}
