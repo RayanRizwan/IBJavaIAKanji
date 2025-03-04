824a57b688d1d6786bed7053322bd4f614820b55
RayanRizwan <63744820+RayanRizwan@users.noreply.github.com>
2025-03-04 08:41:02 -0800

    379842389

---
+++ 2000105_Rizwan_IB_CS_IAKanji/src/Teacher.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author 2000105
+ */
+public class Teacher extends User {
+    
+    public Teacher(String username, String password, String passphrase, String language) {
+        super(username, password, passphrase, language);
+    }
+    
+}
